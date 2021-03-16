71fdcfb9e43e2f08666254a18754156f3cf727b4
rodrigo.diniz <Rodrigo.diniz_ext@s-money.fr>
2021-03-16 12:05:03 +0100

    add fake url

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import useSWR from 'swr';
 const dev = process.env.NODE_ENV !== 'production';
-const server = dev ? 'http://localhost:3000' : '';
+const server = dev ? 'http://localhost:3000' : 'https://curriculo.app.smartmock.io';
 const fetcher = (url) => fetch(url).then((r) => r.json());
 export async function getStaticProps() {
   const curriculo = await fetcher(`${server}/api/curriculo`)
